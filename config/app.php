0cca5c4c11bddb4ba9ee2e8ac8b917ef12e0c544
mujammelhoque <mujammelh111@gmail.com>
2022-03-20 14:51:38 +0600

    new changes

--- config/app.php
+++ config/app.php
@@ -163,10 +163,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
