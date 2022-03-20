bb7fbe372915dd991851f4c6ff7af32981567424
mujammelhoque <mujammelh111@gmail.com>
2022-03-20 20:00:21 +0600

    route resource group

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,16 @@ Route::get('/', function () {
 Auth::routes();
   
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-  
 Route::group(['middleware' => ['auth']], function() {
-    Route::resource('roles', RoleController::class);
-    Route::resource('users', UserController::class);
-    Route::resource('products', ProductController::class);
+    Route::resources([
+        'roles' => RoleController::class,
+        'users' => UserController::class,
+        'products' => ProductController::class,
+    ]);
 });
+  
+// Route::group(['middleware' => ['auth']], function() {
+//     Route::resource('roles', RoleController::class);
+//     Route::resource('users', UserController::class);
+//     Route::resource('products', ProductController::class);
+// });
